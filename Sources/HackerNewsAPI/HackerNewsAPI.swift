b9ead150f583e07cf277afe90358ef0ca136c9a3
Sungbin Jo <pcr910303@icloud.com>
2020-02-01 21:18:21 +0900

    Move parsing logic into it's own class

--- Sources/HackerNewsAPI/HackerNewsAPI.swift
+++ Sources/HackerNewsAPI/HackerNewsAPI.swift
@@ -50,85 +50,12 @@ struct HackerNewsAPI {
             let document = try perform(SwiftSoup.parse(html)) { error in
                 APIError.parsingFailed(error)
             }
-            let fatItem = try unwrap(try! document.select(".fatitem").first(),
-                                     orThrow: APIError.unknown)
-            let rows = try! fatItem.select("table.fatitem > tbody > tr").array()
-            guard rows.count == 2 || rows.count == 4 else {
-                throw APIError.unknown
-            }
-            let itemEl = rows[0]
-            guard itemEl.id() == String(id) else {
-                throw APIError.unknown
-            }
-            let titleAnchor = try unwrap(try! itemEl.select(".storylink").first(),
-                                         orThrow: APIError.unknown)
-            let title = try perform(titleAnchor.text(), orThrow: APIError.unknown)
-            let subTextEl = rows[1]
-            let scoreEl = try! subTextEl.select(".score")
-            let scoreText = try perform(scoreEl.text().split(separator: .space)[0],
-                                        orThrow: APIError.unknown)
-            let score = try unwrap(Int(scoreText), orThrow: APIError.unknown)
-            let authorEl = try! subTextEl.select(".hnuser")
-            let authorName = try perform(authorEl.text(), orThrow: APIError.unknown)
-            var url: URL?
-            var text: String?
-            if rows.count == 2 {
-                // Story with url
-                let urlString = try perform(titleAnchor.attr("href"), orThrow: APIError.unknown)
-                url = URL(string: urlString)
-            } else if rows.count == 4 {
-                // Story with text
-                let textEl = rows[3].child(1)
-                text = try perform(textEl.text(), orThrow: APIError.unknown)
-            }
-            let commentEls = try! document.select(".comtr").array()
-            var commentsPerLevel: [[Comment]] = [[]]
-            for commentEl in commentEls {
-                let currentLevel = commentsPerLevel.count - 1
-                let indentEl = try unwrap(try! commentEl.select(".ind > img").first(),
-                                          orThrow: APIError.unknown)
-                let indentWidth = try perform(indentEl.attr("width"), orThrow: APIError.unknown)
-                let level = try unwrap(Int(indentWidth), orThrow: APIError.unknown) / 40
-                guard level <= currentLevel + 1 else {
-                    throw APIError.unknown
-                }
-                let id = try unwrap(Int(commentEl.id()), orThrow: APIError.unknown)
-                let authorEl = try unwrap(try! commentEl.select(".hnuser").first(),
-                                          orThrow: APIError.unknown)
-                let authorName = try perform(authorEl.text(), orThrow: APIError.unknown)
-                let text = try perform(commentEl.select(".commtext").html(),
-                                       orThrow: APIError.unknown)
-                let comment = Comment(id: id, author: User(creation: Date(), description: nil,
-                                                           name: authorName, karma: 0),
-                                      text: text, comments: [], actions: [])
-                if level <= currentLevel {
-                    while commentsPerLevel.count > level + 1 {
-                        let currentComments = try unwrap(commentsPerLevel.popLast(),
-                                                         orThrow: APIError.unknown)
-                        let currentLevel = commentsPerLevel.count - 1
-                        let preCommentsCount = commentsPerLevel[currentLevel].count
-                        commentsPerLevel[currentLevel][preCommentsCount - 1].comments
-                            = currentComments
-                    }
-                    let currentLevel = commentsPerLevel.count - 1
-                    commentsPerLevel[currentLevel].append(comment)
-                } else {
-                    // level = currentLevel + 1
-                    commentsPerLevel.append([comment])
-                }
-            }
-            while commentsPerLevel.count > 1 {
-                let currentComments = try unwrap(commentsPerLevel.popLast(),
-                                                 orThrow: APIError.unknown)
-                let currentLevel = commentsPerLevel.count - 1
-                let preCommentsCount = commentsPerLevel[currentLevel].count
-                commentsPerLevel[currentLevel][preCommentsCount - 1].comments
-                    = currentComments
-            }
-            guard commentsPerLevel.count == 1 else {
-                throw APIError.unknown
-            }
-            let comments = commentsPerLevel[0]
+            let parser = SiteParser(document: document)
+            let authorName = try parser.authorName()
+            let score = try parser.score()
+            let title = try parser.title()
+            let (url, text) = try parser.content()
+            let comments = try parser.commentTree()
             let promise = firstly {
                 user(withName: authorName)
             }.map { author -> Story in
---
+++ Sources/HackerNewsAPI/SiteParser.swift
@@ -0,0 +1,146 @@
+
+import Foundation
+import SwiftSoup
+
+class SiteParser {
+
+    // MARK: - Error
+
+    enum ParserError: Error {
+        case unknown
+    }
+
+    // MARK: - Stored Properties
+
+    var document: Document
+
+    // MARK: - Computed Properties
+
+    var fatItemEl: Element? {
+        try! document.select(".fatitem").first()
+    }
+    var rowEls: [Element]? {
+        let rowEls = try! fatItemEl?.select("table.fatitem > tbody > tr").array()
+        guard rowEls?.count == 2 || rowEls?.count == 4 else {
+            return nil
+        }
+        return rowEls
+    }
+    var itemEl: Element? {
+        rowEls?[0]
+    }
+    var titleAnchorEl: Element? {
+        try! itemEl?.select(".storylink").first()
+    }
+    var subTextEl: Element? {
+        rowEls?[1]
+    }
+    var hasText: Bool? {
+        rowEls?.count == 4
+    }
+
+    // MARK: - Init
+
+    init(document: Document) {
+        self.document = document
+    }
+
+    // MARK: - Methods
+
+    func title() throws -> String {
+        let titleAnchor = try unwrap(try! itemEl?.select(".storylink").first(),
+                                     orThrow: ParserError.unknown)
+        let title = try perform(titleAnchor.text(), orThrow: ParserError.unknown)
+        return title
+    }
+
+    func score() throws -> Int {
+        let scoreEl = try unwrap(try! subTextEl?.select(".score"),
+                                 orThrow: ParserError.unknown)
+        let scoreText = try perform(scoreEl.text().split(separator: .space)[0],
+                                    orThrow: ParserError.unknown)
+        let score = try unwrap(Int(scoreText), orThrow: ParserError.unknown)
+        return score
+    }
+
+    func authorName() throws -> String {
+        let authorEl = try unwrap(try! subTextEl?.select(".hnuser"),
+                                  orThrow: ParserError.unknown)
+        let authorName = try perform(authorEl.text(), orThrow: ParserError.unknown)
+        return authorName
+    }
+
+    // FIXME: Use proper modeling with enums
+    func content() throws -> (URL?, String?) {
+        var url: URL?
+        var text: String?
+        guard let hasText = hasText else {
+            throw ParserError.unknown
+        }
+        if hasText {
+            let textEl = try unwrap(rowEls?[3].child(1), orThrow: ParserError.unknown)
+            text = try perform(textEl.text(), orThrow: ParserError.unknown)
+        } else {
+            guard let titleAnchorEl = titleAnchorEl else {
+                throw ParserError.unknown
+            }
+            let urlString = try perform(titleAnchorEl.attr("href"),
+                                        orThrow: ParserError.unknown)
+            url = URL(string: urlString)
+        }
+        return (url, text)
+    }
+
+    func commentTree() throws -> [Comment] {
+        let commentEls = try! document.select(".comtr").array()
+        var commentsPerLevel: [[Comment]] = [[]]
+        for commentEl in commentEls {
+            let currentLevel = commentsPerLevel.count - 1
+            let indentEl = try unwrap(try! commentEl.select(".ind > img").first(),
+                                      orThrow: ParserError.unknown)
+            let indentWidth = try perform(indentEl.attr("width"),
+                                          orThrow: ParserError.unknown)
+            let level = try unwrap(Int(indentWidth), orThrow: ParserError.unknown) / 40
+            guard level <= currentLevel + 1 else {
+                throw ParserError.unknown
+            }
+            let id = try unwrap(Int(commentEl.id()), orThrow: ParserError.unknown)
+            let authorEl = try unwrap(try! commentEl.select(".hnuser").first(),
+                                      orThrow: ParserError.unknown)
+            let authorName = try perform(authorEl.text(), orThrow: ParserError.unknown)
+            let text = try perform(commentEl.select(".commtext").html(),
+                                   orThrow: ParserError.unknown)
+            let comment = Comment(id: id, author: User(creation: Date(), description: nil,
+                                                       name: authorName, karma: 0),
+                                  text: text, comments: [], actions: [])
+            if level <= currentLevel {
+                while commentsPerLevel.count > level + 1 {
+                    let currentComments = try unwrap(commentsPerLevel.popLast(),
+                                                     orThrow: ParserError.unknown)
+                    let currentLevel = commentsPerLevel.count - 1
+                    let preCommentsCount = commentsPerLevel[currentLevel].count
+                    commentsPerLevel[currentLevel][preCommentsCount - 1].comments
+                        = currentComments
+                }
+                let currentLevel = commentsPerLevel.count - 1
+                commentsPerLevel[currentLevel].append(comment)
+            } else {
+                // level = currentLevel + 1
+                commentsPerLevel.append([comment])
+            }
+        }
+        while commentsPerLevel.count > 1 {
+            let currentComments = try unwrap(commentsPerLevel.popLast(),
+                                             orThrow: ParserError.unknown)
+            let currentLevel = commentsPerLevel.count - 1
+            let preCommentsCount = commentsPerLevel[currentLevel].count
+            commentsPerLevel[currentLevel][preCommentsCount - 1].comments
+                = currentComments
+        }
+        guard commentsPerLevel.count == 1 else {
+            throw ParserError.unknown
+        }
+        let comments = commentsPerLevel[0]
+        return comments
+    }
+}
